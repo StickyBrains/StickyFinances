bee0b32d313098a352766e2ca1f435d314d21489
StickyBrains <brainssticky@gmail.com>
2025-05-12 19:01:37 +0200

    user was added

---
+++ StickyFinances/User.cs
@@ -0,0 +1,18 @@
+﻿namespace StickyFinances;
+
+public class User
+{
+    private String name;
+    private long id;
+
+    public User()
+    {
+        name = "";
+        id = -1;
+    }
+    public User(String name, long id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+}
